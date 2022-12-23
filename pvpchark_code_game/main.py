995ee6d67cb8aadd90ae4ae6e0114d7840572515
Solution23 <samueldavidriosavila@gmail.com>
2022-12-22 21:45:20 -0500

    Nueva act, esta mejor organizado PD: intenta hacer lo del movimiento y lo de la camara pls

--- pvpchark_code_game/main.py
+++ pvpchark_code_game/main.py
@@ -6,14 +6,12 @@ pantalla = pygame.display.set_mode((width,height))
 img = pygame.image.load(os.path.join("pvpchark_code_game", "Fondo.jpg"))
 imagen_de_fondo = pygame.transform.scale(img, (width,height))
 
-Camara_x = 0
+
 def player(pantalla):
     jugador = pygame.draw.circle(pantalla,(255, 0, 0),(617,699), 20,30)
     pygame.display.flip()
-    #ubicacion = pygame.mouse.get_pos()
-    #print(ubicacion)
 
-def background(Camara_x, imagen_de_fondo): 
+def background(imagen_de_fondo): 
     #Camara_x = -pygame.mouse.get_pos()[0]
     pantalla.blit((imagen_de_fondo), (0,0))
     pygame.display.flip()
@@ -24,9 +22,10 @@ def background(Camara_x, imagen_de_fondo):
 while True:
     for eventos in pygame.event.get():
         if eventos == pygame.QUIT:
-            sys.quit()
+            pygame.quit()
+        #Intenta hacer el manejo del personaje aqui a ver si puedes pls
+
 
     player(pantalla)
-    background(Camara_x, imagen_de_fondo)
-    
+    background(imagen_de_fondo)   
     
