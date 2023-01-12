7812a2ed7bd02afa1336f582bde042ecc3721e03
Solution23 <samueldavidriosavila@gmail.com>
2023-01-12 15:55:16 -0500

    He actualizado todo

--- pvpchark_code_game/main.py
+++ pvpchark_code_game/main.py
@@ -1,31 +1,53 @@
 import pygame
 import os, sys
 
+#Movimiento
+Movimiento_x = 662
+Movimiento_y = 620
+Velocidad_De_Movimiento_x = 0
+Velocidad_De_Movimiento_y = 0
+Movimiento_x = Movimiento_x + Velocidad_De_Movimiento_x
+Movimiento_y = Movimiento_y + Velocidad_De_Movimiento_y
+#Pantalla
 width, height = 1707,915
 pantalla = pygame.display.set_mode((width,height))
-img = pygame.image.load(os.path.join("pvpchark_code_game", "Fondo.jpg"))
+
+#Fondo
+img = pygame.image.load(os.path.join("C:/Users/User/Desktop/PvPChark/pvpchark_code_game","Fondo.jpg"))
 imagen_de_fondo = pygame.transform.scale(img, (width,height))
 
 
-def player(pantalla):
-    jugador = pygame.draw.circle(pantalla,(255, 0, 0),(617,699), 20,30)
+
+#Personaje
+def player(pantalla,Movimiento_x, Movimiento_y):
+    pygame.draw.circle(pantalla,(255, 0, 0),(Movimiento_x,Movimiento_y), 20,30)
     pygame.display.flip()
 
+
+#Fondo
 def background(imagen_de_fondo): 
-    #Camara_x = -pygame.mouse.get_pos()[0]
+    Camara_x = -pygame.mouse.get_pos()[0]
+    Camara_y = -pygame.mouse.get_pos()[0]
     pantalla.blit((imagen_de_fondo), (0,0))
-    pygame.display.flip()
     
-    #print(Camara_x)
+    
+    #print(Camara_x,Camara_y)
+fps = 60
+On = True
+while On:
+    Clock =pygame.time.Clock()
+    Clock.tick(fps)
 
-
-while True:
     for eventos in pygame.event.get():
-        if eventos == pygame.QUIT:
-            pygame.quit()
-        #Intenta hacer el manejo del personaje aqui a ver si puedes pls
+        if eventos.type == pygame.QUIT:
+            On = False
+        if eventos.type == pygame.KEYDOWN:
+            teclas = pygame.key.name(eventos.key)
+            print(teclas)
+            if pygame.key.get_pressed()[pygame.K_w]:
+                Velocidad_De_Movimiento_x = Velocidad_De_Movimiento_x + Velocidad_De_Movimiento_x 
 
 
-    player(pantalla)
-    background(imagen_de_fondo)   
+    player(pantalla,Movimiento_x, Movimiento_y)
+    background(imagen_de_fondo)
     
